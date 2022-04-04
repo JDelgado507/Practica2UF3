20623a39cb6d51d480503a8ffd83b3ccf73a233f
JDelgado507 <javie@DESKTOP-80A7F0P>
2022-04-04 15:40:43 +0200

    Print B

--- Branch_Actividad/src/branch_actividad/Branch_Actividad.java
+++ Branch_Actividad/src/branch_actividad/Branch_Actividad.java
@@ -17,6 +17,8 @@ public class Branch_Actividad {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("A");
+        
+        System.out.println("B");
     }
     
 }
